c5e012e791076f3d9886cdb0cf468bccb53608fd
Filip Woźniak <filip.wozniak98@wp.pl>
2025-01-22 18:19:35 +0100

    added portal to popup

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,3 +1,4 @@
+import {createPortal} from 'react-dom';
 
 
 export default function ResultModal({ ref, remainingTime, targetTime, onReset }) {
@@ -5,7 +6,7 @@ export default function ResultModal({ ref, remainingTime, targetTime, onReset })
 	const formatterRemainTime = remainingTime / 1000;
 	const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
-	return (
+	return (createPortal(
 		<dialog ref={ref} className='result-modal' onClose={onReset}>
 			{youLost && <h2>You Lost</h2>}
 			{!youLost && <h2>Your score {score}</h2>}
@@ -22,6 +23,7 @@ export default function ResultModal({ ref, remainingTime, targetTime, onReset })
 			<form method='dialog' onSubmit={onReset}>
 				<button>Close</button>
 			</form>
-		</dialog>
-	);
+		</dialog>,
+		document.getElementById('modal')
+	));
 }
